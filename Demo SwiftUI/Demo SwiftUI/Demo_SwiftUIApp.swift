4bf0db2b9cb5e79c7f7ebd460f1a671f362d258d
Leonardo Almeida <LeonardoGraselAlmeida@users.noreply.github.com>
2023-02-03 16:23:49 -0300

    init project

---
+++ Demo SwiftUI/Demo SwiftUI/Demo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUIApp.swift
+//  Demo SwiftUI
+//
+//  Created by Leonardo Almeida on 03/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
